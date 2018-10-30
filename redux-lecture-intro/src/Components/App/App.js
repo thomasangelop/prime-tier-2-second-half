2a63112dfe82dc93ffb1ccd07bb681931216fa4b
Thomas Angelo Pinataro <tapinataro@csbsju.edu>
2018-10-30 11:25:14 -0500

    fixed state counter

--- redux-lecture-intro/src/Components/App/App.js
+++ redux-lecture-intro/src/Components/App/App.js
@@ -7,15 +7,30 @@ class App extends Component {
     console.log('button one was clicked');
     this.props.dispatch( { type: 'BUTTON_ONE'} );
   }
+
+  handleClick2 = () => {
+    console.log('button two was clicked');
+    this.props.dispatch( { type: 'BUTTON_TWO'} );
+  }
   
   render() {
+    console.log('Rendering App');
     return (
         <div className="App">
           <h1>Redux Intro</h1>
           <button onClick={this.handleClick}>BUTTON 1</button>
+          <button onClick={this.handleClick2}>BUTTON 2</button>
+          <pre>
+            Junky State:
+            {JSON.stringify( this.props.reduxState )}
+          </pre>
         </div>
     );
   }
 }
 
-export default connect()(App);
+const mapReduxStateToProps = (reduxState) => ({ 
+  reduxState 
+});
+
+export default connect(mapReduxStateToProps)(App);
--- redux-lecture-intro/src/index.js
+++ redux-lecture-intro/src/index.js
@@ -3,25 +3,47 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App/App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-
-// this creates the Redux store
-// It's a big object that holds all our applicaiton's state
-const storeInstance = createStore(
+import logger from 'redux-logger';
 
 // Reducer - It's a function that's going to affect your redux state
 // your reducers should take in state & return the updated state
 // they also should take in an action. 
-// we can choose what to do for specific action types     
-    (state={}, action) => {
-        if(action.type === 'BUTTON_ONE'){
-            console.log('button 1 clicked - reducer');
-        } else {
-            console.log('Hello! I do not care about this action.');
-        }
-        return state;
-    },
+// we can choose what to do for specific action types 
+const buttonOneReducer = (state={clickCount: 0 }, action) => {
+    if(action.type === 'BUTTON_ONE'){
+        console.log('button 1 clicked - reducer');
+        //Add 1 to the state's clickCount
+        state = {clickCount: state.clickCount+=1}; 
+    } else {
+        console.log('Hello! I do not care about this action.');
+    }
+    console.log('state is now', state);
+
+    return state;
+}
+
+const buttonTwoReducer = (state={clickCountB: 0 }, action) => {
+    if(action.type === 'BUTTON_TWO'){
+        console.log('button 2 clicked - reducer');
+        //Add 1 to the state's clickCount
+        state = {clickCountB: state.clickCountB+=1};  
+    } else {
+        console.log('Hello! I do not care about this action.');
+    }
+    console.log('state is now', state);
+
+    return state;
+}
+
+// this creates the Redux store
+// It's a big object that holds all our applicaiton's state
+const storeInstance = createStore(
+    combineReducers({
+        buttonOneReducer, buttonTwoReducer,
+    }),
+    applyMiddleware(logger),
 )
 
 ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
