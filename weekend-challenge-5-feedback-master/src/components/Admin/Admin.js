d6a6fd505ce1b3a9ee2e0e1668faa9eec196245b
Thomas Angelo Pinataro <tapinataro@csbsju.edu>
2018-11-03 12:09:36 -0500

    admin page layout setup

--- weekend-challenge-5-feedback-master/src/components/Admin/Admin.js
+++ weekend-challenge-5-feedback-master/src/components/Admin/Admin.js
@@ -8,6 +8,18 @@ class Admin extends Component {
       <div>
         <h1>Welcome to the Admin Portal for Feedback</h1>
         <h3>Here is the feedback stored:</h3>
+        <div className="Card FeedbackTable">
+          <table>
+            <thead>
+              <th>Feeling</th><th>Comprehension</th><th>Support</th>
+                <th>Comments</th><th>Delete</th>
+            </thead>
+            <tbody>
+              <td>4</td><td>4</td><td>5</td><td>Loved the demo!</td>
+                <td><button>[X]</button></td>
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
