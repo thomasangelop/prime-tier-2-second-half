3bbf541915cf9dc2ac728687077fc875987b1701
Thomas Angelo Pinataro <tapinataro@csbsju.edu>
2018-11-03 11:58:32 -0500

    page 3 layout setup

--- weekend-challenge-5-feedback-master/src/components/Page3/Page3.js
+++ weekend-challenge-5-feedback-master/src/components/Page3/Page3.js
@@ -11,6 +11,16 @@ class Page3 extends Component {
             <h3>3 of 4 pages</h3>
             <h3>[============]</h3>
         </div>
+        <div className="Card QuestionCard">
+            <h4>How well are you being supported?</h4>
+            <p>Please answer on a scale of 1-5.</p>
+            <p>1 being the lowest and 5 being the best.</p>
+            <form>
+                <input type="number" name="Scale 1-5: 
+                    How well are you being supported?" min="1" max="5" required />
+                <input type="submit" value="Next ->" />
+            </form>
+        </div>
       </div>
     );
   }
