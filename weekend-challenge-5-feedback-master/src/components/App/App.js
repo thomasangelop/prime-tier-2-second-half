668d3fd7d07e4937f0f6b8081503e54caeb85efb
Thomas Angelo Pinataro <tapinataro@csbsju.edu>
2018-11-03 11:11:25 -0500

    create file structure for page folders

--- weekend-challenge-5-feedback-master/src/components/App/App.js
+++ weekend-challenge-5-feedback-master/src/components/App/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
+          <h1 className="App-title">*---------Feedback!</h1>
           <h4><i>Don't forget it!</i></h4>
         </header>
         <br/>
