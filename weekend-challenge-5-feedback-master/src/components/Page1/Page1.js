7c0beaa79de29913f53ea3453537705d97858952
Thomas Angelo Pinataro <tapinataro@csbsju.edu>
2018-11-03 11:55:58 -0500

    page 1 layout setup

--- weekend-challenge-5-feedback-master/src/components/Page1/Page1.js
+++ weekend-challenge-5-feedback-master/src/components/Page1/Page1.js
@@ -11,6 +11,16 @@ class Page1 extends Component {
             <h3>1 of 4 pages</h3>
             <h3>[============]</h3>
         </div>
+        <div className="Card QuestionCard">
+            <h4>How are you feeling today?</h4>
+            <p>Please answer on a scale of 1-5.</p>
+            <p>1 being the lowest and 5 being the best.</p>
+            <form>
+                <input type="number" name="Scale 1-5: 
+                    How are you feeling?" min="1" max="5" required />
+                <input type="submit" value="Next ->" />
+            </form>
+        </div>
       </div>
     );
   }
