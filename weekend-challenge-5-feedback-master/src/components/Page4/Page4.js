10b758db7607afb454a1a4fe7884b63cd215f3ca
Thomas Angelo Pinataro <tapinataro@csbsju.edu>
2018-11-03 12:01:18 -0500

    page 4 layout setup

--- weekend-challenge-5-feedback-master/src/components/Page4/Page4.js
+++ weekend-challenge-5-feedback-master/src/components/Page4/Page4.js
@@ -11,6 +11,15 @@ class Page4 extends Component {
             <h3>4 of 4 pages</h3>
             <h3>[============]</h3>
         </div>
+        <div className="Card QuestionCard">
+            <h4>Do you have any comments you want to leave?</h4>
+            <p>Please answer in your own words.</p>
+            <form>
+                <input type="text" name="Do you have any comments you want
+                  to leave?" required />
+                <input type="submit" value="Submit v/ v/" />
+            </form>
+        </div>
       </div>
     );
   }
