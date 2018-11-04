43cc9e2c99ede7a8bd51a72b33ca13d55a837d0b
Thomas Angelo Pinataro <tapinataro@csbsju.edu>
2018-11-04 14:59:28 -0600

    page 3 is sending information

--- weekend-challenge-5-feedback-master/src/components/Page3/Page3.js
+++ weekend-challenge-5-feedback-master/src/components/Page3/Page3.js
@@ -2,7 +2,36 @@ import React, { Component } from 'react';
 import Header from '../Header/Header';
 import { connect } from 'react-redux';
 
+const feedbackInputObject = {
+  feeling: '',
+  comprehension: '',
+  support: '',
+  comments: '',
+}
+
+const mapReduxStateToProps = ( reduxState ) => ({ reduxState });
+
+
 class Page3 extends Component {
+  state = feedbackInputObject;
+
+  handleChange = (event) => {
+    console.log('handling change of support input');
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  handleSubmit = (event) => {
+    console.log('handling submit of support input');
+    event.preventDefault();
+    this.props.dispatch( { type: 'ENTERED_SUPPORT_INFO ', 
+      payload: this.state} );
+    //when form is submitted, this will send us to the next page
+    window.location.hash = "4";
+  }
+
+
   render() {
     return (
       <div>
@@ -15,9 +44,11 @@ class Page3 extends Component {
             <h4>How well are you being supported?</h4>
             <p>Please answer on a scale of 1-5.</p>
             <p>1 being the lowest and 5 being the best.</p>
-            <form>
-                <input type="number" name="Scale 1-5: 
-                    How well are you being supported?" min="1" max="5" required />
+            <form onSubmit={this.handleSubmit} >
+                <input type="number" name="support" min="1" max="5" 
+                onChange={this.handleChange}
+                value={this.state.support} 
+                placeholder="5" required />
                 <input type="submit" value="Next ->" />
             </form>
         </div>
@@ -26,4 +57,4 @@ class Page3 extends Component {
   }
 }
 
-export default connect()(Page3);
+export default connect(mapReduxStateToProps)(Page3);
