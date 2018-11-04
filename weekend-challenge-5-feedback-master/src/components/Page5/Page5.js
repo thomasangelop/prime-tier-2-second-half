d8dd187c0ff76d1eb23e27d9ca35f021e20952ba
Thomas Angelo Pinataro <tapinataro@csbsju.edu>
2018-11-04 15:07:17 -0600

    page 5 is routing back to page 1 if users want to leave more feedback

--- weekend-challenge-5-feedback-master/src/components/Page5/Page5.js
+++ weekend-challenge-5-feedback-master/src/components/Page5/Page5.js
@@ -3,13 +3,20 @@ import Header from '../Header/Header';
 import { connect } from 'react-redux';
 
 class Page5 extends Component {
+
+  handleClick = (event) => {
+    console.log('sending user back to leave new feedback');
+    event.preventDefault();
+    window.location.hash = "";
+  }
+
   render() {
     return (
       <div>
         <Header />
         <div className="Card">
           <h4>Thank you!</h4>
-          <button>Leave New Feedback</button>
+          <button onClick={this.handleClick}>Leave New Feedback</button>
         </div>
       </div>
     );
